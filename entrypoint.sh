9ced4798fc8e41d6c720ba6963ab651a8d3823d7
Julien BONACHERA <jbonachera@xebia.fr>
2018-03-05 16:54:13 +0100

    don't crash on empty strings

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,13 +48,13 @@ ping_kube || exit 1
 
 target=${1:-"/media/template"}
 pattern=$(render.py $target | kubectl -n $KUBE_NAMESPACE apply -f - | grep "deployment")
-if [ pattern != "" ]; then
+if [ "$pattern" != "" ]; then
   name=$(echo $pattern | sed -e 's/deployment "\([^"]\+\)".*/\1/g')
-  if [ -n $name  ]; then
+  if [ -n "$name" ]; then
     kubectl -n $KUBE_NAMESPACE rollout status deployment $name
   fi
   name=$(echo $pattern | sed -e 's/daemonset "\([^"]\+\)".*/\1/g')
-  if [ -n $name  ]; then
+  if [ -n "$name" ]; then
     kubectl -n $KUBE_NAMESPACE rollout status daemonset $name
   fi
 fi
